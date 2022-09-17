47b9ad042559e136fc9c463a7fb6c17fdb8f16ff
Richard <richard_bueno@ymail.com>
2022-09-17 12:45:08 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { ToastContainer } from "react-toastify";
 import Routes from "./routes";
 import GlobalStyles from "./styles/GlobalStyles";
-<<<<<<< HEAD
 import "react-toastify/dist/ReactToastify.css";
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> fbe4840267500f7999d38e62b56229b2f0ff64ab
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
