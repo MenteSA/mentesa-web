e072638ffd9fd642a81640e37fe52fd8bf1d5edd
Alessandra Canuto <ale.canutto@gmail.com>
2022-09-22 19:27:20 -0400

    Alter schedule page

--- src/services/Schedulling/dtos/auth.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface IScheduleDto {
-  id?: number;
-  sessionDate: string;
-  status: 'PENDING' | 'REALIZED' | 'CANCELED';
-  type: 'ONLINE' | 'PRESENTIAL';
-  scheduleType: 'INDIVIDUAL' | 'COUPLE' | 'IN_GROUP';
-}
